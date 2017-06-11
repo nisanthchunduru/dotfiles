27eef1788dd1bfc99962a295081fd7658d5c9e16
Nisanth Chunduru <nisanth074@gmail.com>
2017-06-12 01:16:19 +0530

    Add a ruby script to backup dotfiles into the ~/.dotfiles dir

---
+++ backup_dotfiles.rb
@@ -0,0 +1,19 @@
+class DotfileInfo
+  def initialize(dotfile_info_string)
+  	@dotfile_info_string = dotfile_info_string
+  end
+
+  def path
+  	@dotfile_info_string.split(":")[0]
+  end
+
+  def backup_path
+  	@dotfile_info_string.split(":")[1]
+  end
+end
+
+dotfile_info_strings = `lsrc`.split("\n")
+dotfile_info_strings.each do |dotfile_info_string|
+  dotfile_info = DotfileInfo.new(dotfile_info_string)
+  `cp #{dotfile_info.path} #{dotfile_info.backup_path}` if File.exist?(dotfile_info.path)
+end
