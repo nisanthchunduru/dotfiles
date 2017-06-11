eca00bd73d401e8f457dc559b27716472af0db78
Nisanth Chunduru <nisanth074@gmail.com>
2017-06-12 01:25:17 +0530

    Fix issue: cp command fails if dotfile and backup dotfile are identical

--- backup_dotfiles.rb
+++ backup_dotfiles.rb
@@ -15,5 +15,5 @@ end
 dotfile_info_strings = `lsrc`.split("\n")
 dotfile_info_strings.each do |dotfile_info_string|
   dotfile_info = DotfileInfo.new(dotfile_info_string)
-  `cp #{dotfile_info.path} #{dotfile_info.backup_path}` if File.exist?(dotfile_info.path)
+  FileUtils.cp(dotfile_info.path, dotfile_info.backup_path) if File.exist?(dotfile_info.path)
 end
