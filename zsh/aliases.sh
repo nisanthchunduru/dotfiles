9a9884e0bbcf2e88d8fa4adb97a56e25a297d035
Nisanth Chunduru <nisanth074@gmail.com>
2018-01-03 18:16:11 +0530

    Add an alias to quickly symlink new dotfiles added to the ~/.dotfiles directory

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,5 +1,12 @@
-alias l=ls
+alias symlink_dotfiles='rcup -d ~/.dotfiles/ -K'
 
+alias l=ls
+alias diff=colordiff
+alias cask='brew cask'
+alias r=rvm
+alias pub='cat ~/.ssh/id_rsa.pub | tee >(pbcopy)' # Print ssh public key
+
+# Git
 alias g=git
 alias gs='git status'
 alias gcl='git clone'
@@ -13,11 +20,7 @@ alias gcp='git cherry-pick'
 alias gri='git rebase -i'
 alias gf='git fetch'
 
-alias x=extract
-alias diff=colordiff
-alias cask='brew cask'
-alias r=rvm
-
+# Bundler
 alias b=bundle
 alias be='bundle exec'
 alias bi='bundle install --path vendor/bundle'
@@ -25,23 +28,25 @@ alias bs='bundle show'
 alias bcd='bundle-cd'
 alias bo='EDITOR=atom bundle open'
 
+# Rails
 alias run_migrations='bundle exec rake db:migrate'
 alias revert_migration='bundle exec rake db:rollback STEP=1'
+# https://github.com/mperham/sidekiq/wiki/Problems-and-Troubleshooting#my-sidekiq-process-is-crashing-what-do-i-do
+alias print_native_gems="bundle exec ruby -e 'puts Gem.loaded_specs.values.select{ |i| !i.extensions.empty? }.map{ |i| i.name }'"
 
 alias invoker='NOEXEC_DISABLE=1 rvm default do invoker'
 alias rerun='NOEXEC_DISABLE=1 rvm default do rerun'
 
-alias pub='cat ~/.ssh/id_rsa.pub | tee >(pbcopy)'
-
+# Ansible
 # alias an=ansible
 # alias anp=ansible-playbook
+
+# Applications
 alias sourcetree='open -a SourceTree'
 alias macdown='open -a MacDown'
 alias st=sourcetree .
 alias typora='open -a Typora'
 
+# DNS
 alias use_google_dns='networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4'
 alias use_adguard_dns='networksetup -setdnsservers Wi-Fi 176.103.130.130 176.103.130.131'
-
-# https://github.com/mperham/sidekiq/wiki/Problems-and-Troubleshooting#my-sidekiq-process-is-crashing-what-do-i-do
-alias print_native_gems="bundle exec ruby -e 'puts Gem.loaded_specs.values.select{ |i| !i.extensions.empty? }.map{ |i| i.name }'"
