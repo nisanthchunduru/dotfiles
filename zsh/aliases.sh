2669c065daff974c6d59a18c12a48989fadd8aff
Nisanth Chunduru <nisanth074@gmail.com>
2018-04-04 13:36:00 +0530

    Add alias to use cloudflare dns

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -57,4 +57,6 @@ alias typora='open -a Typora'
 
 # DNS
 alias use_google_dns='networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4'
+alias use_cloudflare_dns='networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1'
 alias use_adguard_dns='networksetup -setdnsservers Wi-Fi 176.103.130.130 176.103.130.131'
+
