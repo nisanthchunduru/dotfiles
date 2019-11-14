d4159fe4b9b0b28393fbc98b5d62b323f87d95b3
Nisanth Chunduru <nisanth074@gmail.com>
2019-11-14 14:28:37 +0530

    Add an alias to easily download the subtitle for any movie

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,7 +7,7 @@ alias l=ls
 alias diff=colordiff
 alias cask='brew cask'
 alias pub='cat ~/.ssh/id_rsa.pub | tee >(pbcopy)' # Print ssh public key
-alias subit='subdb d'
+alias sub='subdb d'
 
 # Git
 alias g=git
