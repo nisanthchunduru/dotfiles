e50dc55f9dd51f5deeeb1f1d84f45a7e69875190
Nisanth Chunduru <chunisan@amazon.com>
2025-02-25 03:11:58 +0530

    Remove alias for webstorm

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -27,7 +27,6 @@ alias brs='brew search'
 alias co='code'
 alias cu='cursor'
 alias ws='windsurf'
-alias wst='webstorm'
 alias sourcetree='open -a SourceTree'
 alias st=sourcetree .
 alias macdown='open -a MacDown'
