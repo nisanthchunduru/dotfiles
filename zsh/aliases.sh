7671d3d16886ec03d7b482b0d5db639e50bd07ca
Nisanth Chunduru <chunisan@amazon.com>
2025-11-17 19:30:19 +0530

    Modify `light-mode` and `dark-mode` aliases to also change Kiro's theme

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -15,8 +15,8 @@ alias symlink_dotfiles='rcup -d ~/.dotfiles/ -K'
 # MacOS
 # alias dark-mode='defaults write -g AppleInterfaceStyle -string "Dark"'
 # alias light-mode='defaults write -g AppleInterfaceStyle -string "Light"'
-alias dark-mode='osascript -e '\''tell app "System Events" to tell appearance preferences to set dark mode to true'\'''
-alias light-mode='osascript -e '\''tell app "System Events" to tell appearance preferences to set dark mode to false'\'''
+alias dark-mode='osascript -e '\''tell app "System Events" to tell appearance preferences to set dark mode to true'\'' && sed -i "" "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"GitHub Dark\"/" ~/Library/Application\ Support/Kiro/User/settings.json'
+alias light-mode='osascript -e '\''tell app "System Events" to tell appearance preferences to set dark mode to false'\'' && sed -i "" "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"GitHub Light\"/" ~/Library/Application\ Support/Kiro/User/settings.json'
 
 # Applications
 alias co='code'
