095d53d8e3ad087b981dd42b20baa3f75d8d7c3c
Nisanth Chunduru <chunisan@amazon.com>
2026-01-25 20:07:56 +0530

    Rename alias `brb` as `brbug`

--- zsh/aliases/homebrew-aliases.sh
+++ zsh/aliases/homebrew-aliases.sh
@@ -1,6 +1,7 @@
-alias brb='brew bundle --global'
-alias ibrew='arch -x86_64 /usr/local/bin/brew'
 alias bri='brew install'
 alias brs='brew search'
+alias brbug='brew bundle --global'
 
 alias cask='brew cask'
+
+alias ibrew='arch -x86_64 /usr/local/bin/brew'
