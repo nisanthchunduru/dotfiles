bc116cf105b07ad8cb4dce0d59c73e071f8a3921
Nisanth Chunduru <chunisan@amazon.com>
2026-05-18 17:23:49 +0530

    Add a function to print or change dock icon size

--- zsh/aliases/mac-aliases.sh
+++ zsh/aliases/mac-aliases.sh
@@ -4,3 +4,11 @@ alias dark-mode='osascript -e '\''tell app "System Events" to tell appearance pr
 alias light-mode='osascript -e '\''tell app "System Events" to tell appearance preferences to set dark mode to false'\'' && sed -i "" "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"GitHub Light\"/" ~/Library/Application\ Support/Kiro/User/settings.json'
 
 alias intel='arch -x86_64'
+
+dock-icon-size() {
+  if [ -z "$1" ]; then
+    echo "Current Dock icon size: $(defaults read com.apple.dock tilesize)"
+  else
+    defaults write com.apple.dock tilesize -int "$1" && killall Dock
+  fi
+}
