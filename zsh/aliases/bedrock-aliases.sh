19b605b627f4f95e160870be2076e0af9d8f3f84
Nisanth Chunduru <chunisan@amazon.com>
2026-05-19 15:48:56 +0530

    Rename alias `bedrock-models` as `bedrock-models-ids`

--- zsh/aliases/bedrock-aliases.sh
+++ zsh/aliases/bedrock-aliases.sh
@@ -1,1 +1,1 @@
-alias bedrock-models='aws bedrock list-foundation-models --query "modelSummaries[].modelId" --region us-east-1 --output text | tr "\t" "\n"'
+alias bedrock-models-ids='aws bedrock list-foundation-models --query "modelSummaries[].modelId" --region us-east-1 --output text | tr "\t" "\n"'
