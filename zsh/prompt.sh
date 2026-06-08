c2cbf39e856a19a46cd3ac404cc8c1d6068c826b
Nisanth Chunduru <chunisan@amazon.com>
2026-06-09 02:40:28 +0530

    Temporarily move to a synchronous prompt so zsh displays errors when a command fails

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,98 +1,23 @@
-# Lightweight prompt with async Git status.
-#
-# Renders like:
-#   ~/repos/dotfiles master*
-#
-# Git state is computed after the prompt appears, then zle refreshes the prompt
-# when the background process returns.
+autoload -U colors && colors
 
 setopt prompt_subst
 
-autoload -Uz add-zsh-hook
+function git_branch_info {
+  command git rev-parse --is-inside-work-tree >/dev/null 2>&1 || return
 
-typeset -g ZSH_GIT_PROMPT=''
-typeset -g _ZSH_GIT_PROMPT_FD=''
-typeset -g _ZSH_GIT_PROMPT_CWD=''
+  local branch=''
+  local dirty=''
 
-PROMPT='%~${ZSH_GIT_PROMPT} '
-
-function _zsh_git_prompt_close_fd {
-  emulate -L zsh
-
-  local fd=$_ZSH_GIT_PROMPT_FD
-  [[ -n $fd ]] || return
-
-  zle -F $fd 2>/dev/null
-  exec {fd}<&- 2>/dev/null
-  _ZSH_GIT_PROMPT_FD=''
-}
-
-function _zsh_git_prompt_done {
-  emulate -L zsh
-
-  local fd=$1
-  local prompt_segment=''
-
-  if IFS= read -r prompt_segment <&$fd; then
-    if [[ $PWD == $_ZSH_GIT_PROMPT_CWD ]]; then
-      ZSH_GIT_PROMPT=$prompt_segment
-    fi
+  branch=$(command git symbolic-ref --quiet --short HEAD 2>/dev/null)
+  if [[ -z $branch ]]; then
+    branch=$(command git rev-parse --short HEAD 2>/dev/null) || return
   fi
 
-  if [[ $_ZSH_GIT_PROMPT_FD == $fd ]]; then
-    _zsh_git_prompt_close_fd
-  else
-    zle -F $fd 2>/dev/null
-    exec {fd}<&- 2>/dev/null
+  if [[ -n $(command git status --porcelain --untracked-files=normal 2>/dev/null) ]]; then
+    dirty='*'
   fi
 
-  zle reset-prompt 2>/dev/null
+  print -r -- " %{$fg[red]%}[${branch}${dirty}]%{$reset_color%}"
 }
 
-function _zsh_git_prompt_start {
-  emulate -L zsh
-
-  [[ -o interactive ]] || return
-
-  _zsh_git_prompt_close_fd
-  _ZSH_GIT_PROMPT_CWD=$PWD
-
-  exec {_ZSH_GIT_PROMPT_FD}< <(
-    emulate -L zsh
-
-    local branch=''
-    local dirty=''
-
-    command git rev-parse --is-inside-work-tree >/dev/null 2>&1 || exit 0
-
-    branch=$(command git symbolic-ref --quiet --short HEAD 2>/dev/null)
-    if [[ -z $branch ]]; then
-      branch=$(command git rev-parse --short HEAD 2>/dev/null) || exit 0
-    fi
-
-    if [[ -n $(command git status --porcelain --untracked-files=normal 2>/dev/null) ]]; then
-      dirty='*'
-    fi
-
-    print -r -- " ${branch}${dirty}"
-  )
-
-  zle -F $_ZSH_GIT_PROMPT_FD _zsh_git_prompt_done 2>/dev/null || _zsh_git_prompt_close_fd
-}
-
-function _zsh_git_prompt_precmd {
-  emulate -L zsh
-
-  if [[ $PWD != $_ZSH_GIT_PROMPT_CWD ]]; then
-    ZSH_GIT_PROMPT=''
-  fi
-
-  _zsh_git_prompt_start
-}
-
-function _zsh_git_prompt_chpwd {
-  ZSH_GIT_PROMPT=''
-}
-
-add-zsh-hook precmd _zsh_git_prompt_precmd
-add-zsh-hook chpwd _zsh_git_prompt_chpwd
+PROMPT='%~$(git_branch_info)%# '
