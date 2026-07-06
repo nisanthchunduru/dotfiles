8ef9325b5d1253f27b30702c97f45964b015d327
Nisanth Chunduru <nisanth074@gmail.com>
2026-07-06 18:49:42 +0530

    Add an alias to flush MacOS DNS cache

--- zsh/aliases/dns-aliases.sh
+++ zsh/aliases/dns-aliases.sh
@@ -2,3 +2,5 @@ alias use-google-dns='networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4'
 alias use-cloudflare-dns='networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1'
 alias use-adguard-dns='networksetup -setdnsservers Wi-Fi 176.103.130.130 176.103.130.131'
 alias use-local-dns='networksetup -setdnsservers Wi-Fi 127.0.0.1'
+
+alias flush-dns-cache='sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
